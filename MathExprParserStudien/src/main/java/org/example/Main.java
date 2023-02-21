4b56fa1a8e3d87a4c09fd2d71dce9384ac860811
reichert-p <73558016+reichert-p@users.noreply.github.com>
2023-02-21 22:00:34 +0100

    adapt main to parse basic example

--- MathExprParserStudien/src/main/java/org/example/Main.java
+++ MathExprParserStudien/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import com.oracle.truffle.api.CallTarget;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.example.Nodes.MathExprNode;
+import org.example.Nodes.MathExprRootNode;
+import org.example.Parsing.Gen.MathExprParser;
+import org.example.Parsing.Gen.MathExprLexer;
+import org.example.Parsing.MathExprTruffleParser;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MathExprTruffleParser parser = new MathExprTruffleParser();
+        MathExprNode men = parser.parse("1+2");
+
+        var rootNode = new MathExprRootNode(men);
+        CallTarget callTarget = rootNode.getCallTarget();
+
+        var result = callTarget.call();
+
+        System.out.println(result);
     }
 }
